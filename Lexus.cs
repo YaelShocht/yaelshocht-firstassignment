23ebddc97486d77a3b83a65d97c6bf92ac04bfeb
YaelShocht <126409438+YaelShocht@users.noreply.github.com>
2023-10-28 20:19:31 +0300

    Update Lexus.cs
    Update Lexus

--- Lexus.cs
+++ Lexus.cs
@@ -1,16 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-//CR - remove all unused packages
 
 namespace ConsoleApp2
 {
     internal class Lexus:Car
     {
-        public bool IsPlayingMusic { get; private set; } 
-        //CR - why not private? also - why do you need a property if you created StartMusic() and StopMusic() for changing the value of this field?
+    privat bool IsPlayingMusic;
+        
         public Lexus(string model): base(model) {
             IsPlayingMusic = false;
         }
